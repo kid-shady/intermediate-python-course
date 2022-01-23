9f60c14a6497a7ee7c52c07d4d456e1b20891cdd
kidwest <kidwest754@gmail.com>
2022-01-23 11:20:18 +0530

    Revert "Tutorial complete"
    This reverts commit 4ec4ccb03cbaf0c71f2cc8de7b54f004f8ac0e67.

--- dice_roller.py
+++ dice_roller.py
@@ -1,17 +1,7 @@
 import random
 def main():
-  dice_rolls=3
-  dice_sums=0
-  for i in range(0,dice_rolls):     
-    roll=random.randint(1,6)
-    dice_sums+=roll
-    print(f'you rolled a {roll}')
-    if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail')
-    elif roll == 6:
-      print(f'You rolled a {roll}! Critical Success!')
-    else:
-      print(f'You rolled a {roll}')
-  print(f'you rolled a total of {dice_sums}')
+  roll=random.randint(1,6)
+  print(f'you rolled a {roll}')
+  
 if __name__== "__main__":
   main()
